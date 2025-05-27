afe468fa5fcbbb89fdc309135892441cabcbcba1
Wintersnek <80545731+Wintersnek@users.noreply.github.com>
2025-05-27 17:54:28 +0200

    Samlade upp alla nya funktioner

--- test/src/SDGSweden/Start.java
+++ test/src/SDGSweden/Start.java
@@ -4,18 +4,17 @@ import oru.inf.InfDB;
 import oru.inf.InfException;
 
 public class Start {
-    
+
     private static InfDB idb;
 
     public static void main(String[] args) {
         try {
             idb = new InfDB("SDGSweden", "3306", "dbAdmin2024", "dbAdmin2024PW");
-            
-            System.out.println("funkar");
+
         } catch (InfException ex) {
             System.out.println(ex.getMessage());
         }
-        
+
         new Inloggning(idb).setVisible(true);
     }
 }
